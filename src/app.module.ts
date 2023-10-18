14a4e5956656dec25c09a80169a0f5475f8988c5
Gabriel <gabriel.merigo.dev@gmail.com>
2023-10-18 11:55:12 -0300

    using RS256 to authenticate user

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,20 @@
-import { Module } from "@nestjs/common";
-import { CreateAccountController } from "./controllers/create-account.controller";
-import { PrismaService } from "./prisma/prisma.service";
-
+import { Module } from '@nestjs/common'
+import { ConfigModule } from '@nestjs/config'
+import { AuthModule } from './auth/auth.module'
+import { CreateAccountController } from './controllers/create-account.controller'
+import { envSchema } from './env'
+import { PrismaService } from './prisma/prisma.service'
+import { AuthenticateController } from './controllers/authenticate.controller'
 
 @Module({
-  imports: [],
-  controllers: [CreateAccountController],
+  imports: [
+    ConfigModule.forRoot({
+      validate: (env) => envSchema.parse(env),
+      isGlobal: true,
+    }),
+    AuthModule,
+  ],
+  controllers: [CreateAccountController, AuthenticateController],
   providers: [PrismaService],
 })
 export class AppModule {}
--- src/controllers/create-account.controller.ts
+++ src/controllers/create-account.controller.ts
@@ -1,23 +1,30 @@
-import { Body, ConflictException, Controller, HttpCode, Post, UsePipes } from '@nestjs/common';
-import { hash } from 'bcryptjs';
-import { z } from 'zod';
+import {
+  Body,
+  ConflictException,
+  Controller,
+  HttpCode,
+  Post,
+  UsePipes,
+} from '@nestjs/common'
+import { hash } from 'bcryptjs'
+import { z } from 'zod'
 
-import { ZodValidationPipe } from 'src/pipes/zod.validation.pipe';
-import { PrismaService } from 'src/prisma/prisma.service';
+import { ZodValidationPipe } from 'src/pipes/zod.validation.pipe'
+import { PrismaService } from 'src/prisma/prisma.service'
 
-const SALT = 8;
+const SALT = 8
 
 const createAccountBodySchema = z.object({
   name: z.string(),
   email: z.string().email(),
-  password: z.string()
+  password: z.string(),
 })
 
-type CreateAccountBodySchema = z.infer<typeof createAccountBodySchema>;
+type CreateAccountBodySchema = z.infer<typeof createAccountBodySchema>
 
 @Controller('/accounts')
 export class CreateAccountController {
-  constructor (private prisma: PrismaService) {}
+  constructor(private prisma: PrismaService) {}
 
   @Post()
   @HttpCode(201)
@@ -27,22 +34,24 @@ export class CreateAccountController {
 
     const userWithSameEmail = await this.prisma.user.findUnique({
       where: {
-        email
-      }
+        email,
+      },
     })
 
-    if(userWithSameEmail) {
-     throw new ConflictException('User with same e-mail address already exists.') 
+    if (userWithSameEmail) {
+      throw new ConflictException(
+        'User with same e-mail address already exists.',
+      )
     }
 
-    const hashedPassword = await hash(password, SALT);
+    const hashedPassword = await hash(password, SALT)
 
     await this.prisma.user.create({
       data: {
         name,
         email,
-        password: hashedPassword
-      }
+        password: hashedPassword,
+      },
     })
   }
 }
